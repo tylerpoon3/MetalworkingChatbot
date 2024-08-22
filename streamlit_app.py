dcf1a98be22567b105568b4d6150bc940c013ac8
tylerpoon3 <tylerpoon3@gmail.com>
2024-08-22 15:13:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import numpy as np
 
 with st.chat_message("user"):
     st.write("Hello 👋")
+
+with st.chat_messge("assistant"):
+    st.write("Hello human")
+    st.bar_chart(np.random.randn(30,3))
