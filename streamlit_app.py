a72b91b7ffa02816c1c83d8b1e05e94bc969ef4b
tylerpoon3 <tylerpoon3@gmail.com>
2024-08-22 15:17:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 import streamlit as st
-import numpy as np
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
+st.title("Echo Bot")
 
-with st.chat_message("assistant"):
-    st.write("Hello human")
-    st.bar_chart(np.random.randn(30, 3))
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
