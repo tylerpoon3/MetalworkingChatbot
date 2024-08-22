5594267e40100d8f3e7b05180b5f3597e7a0dfa7
tylerpoon3 <tylerpoon3@gmail.com>
2024-08-22 15:15:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ with st.chat_message("user"):
 with st.chat_message("assistant"):
     st.write("Hello human")
     st.bar_chart(np.random.randn(30, 3))
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
