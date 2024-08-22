6e431a51c43a5bea6553831790d211a6e27649cd
tylerpoon3 <tylerpoon3@gmail.com>
2024-08-22 15:27:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import random
+import time
 
-st.title("Echo Bot")
+st.title("Simple chat")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -19,9 +21,20 @@ if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     
-    response = f"Echo: {prompt}"
+   #Streamed response emulator
+    def response_generator():
+        repsonse = random.choice(
+            [
+                "Hello there! How can I assist you today?",
+                "Hi human! Is there anything I can help you with?",
+                "Do you need help?",
+            ]
+        )
+        for word in response.split():
+            yield word + " "
+            time.sleep(0.05)
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        st.markdown(response)
+        response = st.write_stream(response_generator())
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
