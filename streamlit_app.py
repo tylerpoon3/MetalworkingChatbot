f3d0e7a0a5c4d1f35b57afc3e7174cfd42991d58
tylerpoon3 <tylerpoon3@gmail.com>
2024-08-22 15:29:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,20 @@ import streamlit as st
 import random
 import time
 
- #Streamed response emulator
-    def response_generator():
-        repsonse = random.choice(
-            [
-                "Hello there! How can I assist you today?",
-                "Hi human! Is there anything I can help you with?",
-                "Do you need help?",
-            ]
-        )
-        for word in response.split():
-            yield word + " "
-            time.sleep(0.05)
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
 
 st.title("Simple chat")
 
@@ -26,14 +28,14 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# React to user input
+# Accept user input
 if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    
+
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         response = st.write_stream(response_generator())
